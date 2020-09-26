0618e70f5a6d896d762839d74fb5162e22febc88
Jens Klingenberg <mail@jensklingenberg.de>
2020-09-26 10:48:39 +0200

    Revert Gradle/Kotlin version, change copyright message, remove checkbox

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ allprojects {
 }
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.0"
     id("io.gitlab.arturbosch.detekt") version "1.13.1"
     id("org.jlleitschuh.gradle.ktlint") version "9.4.0"
 }
--- recompose-idea/src/main/kotlin/recompose/plugin/copypaste/ConvertXmlToComposeConfirmationDialog.kt
+++ recompose-idea/src/main/kotlin/recompose/plugin/copypaste/ConvertXmlToComposeConfirmationDialog.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2020 Sebastian Kaspari
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,18 +18,16 @@ import com.intellij.CommonBundle
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import java.awt.Container
-import javax.swing.JCheckBox
+import javax.swing.Action
 import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JPanel
-import javax.swing.Action
 
 /**
  * This dialog asks if the user wants to convert the pasted Xml to Compose
  */
-class KotlinPasteFromXmlDialog(project: Project) : DialogWrapper(project, true) {
+class ConvertXmlToComposeConfirmationDialog(project: Project) : DialogWrapper(project, true) {
     lateinit var panel: JPanel
-    @Suppress("unused") lateinit var donTShowThisCheckBox: JCheckBox
     @Suppress("unused") lateinit var questionLabel: JLabel
 
     init {
