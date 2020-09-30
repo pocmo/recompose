9a0c15d14cbf6f815cc8d7193a67feeda63378e4
Jens Klingenberg <mail@jensklingenberg.de>
2020-09-30 20:15:43 +0200

    Issue #8: Support pasting layout XML from outside of IntelliJ / Android Studio
    Closes #8.

--- recompose-idea/src/main/kotlin/recompose/plugin/copypaste/RecomposeCopyPasteProcessor.kt
+++ recompose-idea/src/main/kotlin/recompose/plugin/copypaste/RecomposeCopyPasteProcessor.kt
@@ -29,6 +29,7 @@ import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.idea.KotlinFileType
 import recompose.composer.Composer
 import recompose.parser.Parser
+import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.Transferable
 
 /**
@@ -58,14 +59,28 @@ class RecomposeCopyPasteProcessor : CopyPastePostProcessor<TextBlockTransferable
     override fun extractTransferableData(
         content: Transferable
     ): List<TextBlockTransferableData> {
-        if (content.isDataFlavorSupported(CopiedXMLCode.DATA_FLAVOR)) {
-            // There's some matching data in this paste, let's return it to process it.
-            return listOf(
-                content.getTransferData(CopiedXMLCode.DATA_FLAVOR) as TextBlockTransferableData
-            )
-        }
 
-        return emptyList()
+        return if (content.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+            val text = content.getTransferData(DataFlavor.stringFlavor) as String
+            val xmlPreamble = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+
+            when {
+                content.isDataFlavorSupported(CopiedXMLCode.DATA_FLAVOR) -> {
+                    // There's some matching data in this paste, let's return it to process it.
+                    listOf(
+                        content.getTransferData(CopiedXMLCode.DATA_FLAVOR) as TextBlockTransferableData
+                    )
+                }
+                text.contains(xmlPreamble, true) -> {
+                    listOf(CopiedXMLCode(text, intArrayOf(0), intArrayOf(0)) as TextBlockTransferableData)
+                }
+                else -> {
+                    emptyList()
+                }
+            }
+        } else {
+            emptyList()
+        }
     }
 
     // Perform paste: Process transferable data
